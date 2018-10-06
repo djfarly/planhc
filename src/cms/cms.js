f64b95fd6f91eb9d098b9b3b5d0f1ccaacdfb6cb
Jan Willem Henckel <jan@mpfilm.de>
2018-10-06 21:06:36 +0200

    fix cms type

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,9 +1,9 @@
 import CMS from 'netlify-cms';
 
 import AboutPagePreview from './preview-templates/AboutPagePreview';
-import ProjectPreview from './preview-templates/ProjectPreview';
+import ProjektPreview from './preview-templates/ProjektPreview';
 import ProductPagePreview from './preview-templates/ProductPagePreview';
 
 CMS.registerPreviewTemplate('about', AboutPagePreview);
 CMS.registerPreviewTemplate('products', ProductPagePreview);
-CMS.registerPreviewTemplate('projects', ProjectPreview);
+CMS.registerPreviewTemplate('projekte', ProjektPreview);
